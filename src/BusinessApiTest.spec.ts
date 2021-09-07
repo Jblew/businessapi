3ea317927b49074beb078169303615a5b40ff096
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-09-07 17:35:41 +0200

    test: tests for BusinessApiTest.fakeService

--- src/BusinessApiTest.spec.ts
+++ src/BusinessApiTest.spec.ts
@@ -1,13 +1,43 @@
 import { expect } from "chai";
+import { BusinessApiTest } from "./BusinessApiTest";
 
 describe("BusinessApiTest", () => {
-  it("Specify structure, behaviour and tests for BusinessApiTest mocking");
-
+  const validEmployee = {
+    firstName: "a",
+    lastName: "b",
+    username: "c",
+    rules: [],
+  };
   describe("fakeService", () => {
-    it("Creates service that can be accessed via env name");
-    it("Calls handler");
-    it("Returns handler response to the call()");
-    it("Resolves with request body");
+    it("Creates service that can be accessed via env name", () => {});
+
+    it("Calls handler with GET method", async () => {
+      const test = new BusinessApiTest();
+      const [request, response] = await Promise.all([
+        test.fakeService("SERVICE_URL_EMPLOYEE", async (_) => validEmployee),
+        test.call("SERVICE_URL_EMPLOYEE").responseSchema<any>("Employee").get(),
+      ]);
+      expect(request).to.be.undefined;
+      expect(response.firstName).to.be.equal(validEmployee.firstName);
+    });
+
+    it("Passes POST body to the handler", async () => {
+      const test = new BusinessApiTest();
+      let passedRequest: any;
+      const [request, response] = await Promise.all([
+        test.fakeService("SERVICE_URL_EMPLOYEE", async (body: any) => {
+          passedRequest = body;
+          return validEmployee;
+        }),
+        test
+          .call("SERVICE_URL_EMPLOYEE")
+          .requestSchema<any>("Employee")
+          .responseSchema<any>("Employee")
+          .post(validEmployee),
+      ]);
+      expect(request.firstName).to.be.equal(validEmployee.firstName);
+      expect(passedRequest.firstName).to.be.equal(validEmployee.firstName);
+    });
   });
 
   describe("fakeCall", () => {
